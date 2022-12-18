503c719c741406e54bfe5c94fd8ed7989aee4a7b
chinasa <njokuchinasa86@gmail.com>
2022-12-18 06:00:58 +0100

    eight commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,11 +124,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 
@@ -136,5 +131,12 @@ STATICFILES_DIRS = [
     
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+
+
 MEDIA_URL = "/media/"
 MEDIAL_ROOT = os.path.join(BASE_DIR,"media/")
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
